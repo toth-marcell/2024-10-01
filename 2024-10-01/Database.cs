3f958135eb3247279d6482d5a160fbe00be16780
toth-marcell <toth.marcell.21i@egressy.info>
2024-10-01 09:12:18 +0200

    formatting

--- 2024-10-01/Database.cs
+++ 2024-10-01/Database.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.Windows.Forms;
+
 namespace _2024_10_01
 {
     public static class Database
